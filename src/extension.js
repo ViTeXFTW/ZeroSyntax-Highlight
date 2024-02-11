6bb9c8aceda1f8df508dbeee2ed04f23d4d557a4
ViTeXFTW <mads213@gmail.com>
2024-02-11 18:25:19 +0100

    Added playertemplate and partifclesystem. Numbers now check for color or pos prefix

---
+++ src/extension.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
+function activate(context) {
+    var test = 2;
+}
+exports.activate = activate;
---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+    let test = 2;
+
+
+}
