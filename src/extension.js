854a88d4e52bab2165179b5e3b74a013428c4004
Mads Rasmussen <s215521@dtu.dk>
2024-03-06 11:05:03 +0100

    Added: New packaged extension v1.0.0 Changed: version in package.json Removed: extension.ts/js used for testing.

--- src/extension.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.activate = void 0;
-function activate(context) {
-    var test = 2;
-}
-exports.activate = activate;
--- src/extension.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-
-
-export function activate(context: vscode.ExtensionContext) {
-
-    let test = 2;
-
-
-}
